7cdd984d24607c0267d71aa1207ecf48cb5f9eae
Ayano <ayanotarumi@gmail.com>
2017-12-28 22:37:42 +0100

    [day2 part2] Didn't find a way.. Create a function to calculate a cartesian product. Then try to find 2 good numbers.

---
+++ day2_part2.py
@@ -0,0 +1,37 @@
+def check_row(row):
+	# pour chaque ligne, trouver le produit cartesian
+	pass
+
+
+def checksum(rows):
+	rows = ([int(r) for r in row.strip().split()] for row in rows)
+	# diff = (max(row) - min(row) for row in rows if row)
+
+
+	return sum(check_row(row) for row in rows)
+
+	# for row in rows:
+
+	# 	row.sort()
+	# 	for r in row:
+	# 		if row[:row.index(r):-1] % r == 0:
+	# 			row[:row.index(r):-1] // r
+			
+
+
+	# 	print(row)
+
+	# return sum(diff)
+	# return rows
+
+
+def day2(path):
+	with open(path) as f:
+		content = f.readlines()
+	
+	result = checksum(content)
+	print(result)
+
+
+if __name__ == '__main__':
+	day2('./day2_input1.d')
